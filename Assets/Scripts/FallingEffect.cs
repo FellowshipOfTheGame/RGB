2f9af35028122a259272f0e8cd4b4a20ed456141
Giovanni Paolo Meloni <gio.mel@hotmail.com>
2018-12-06 15:12:57 -0200

    Criado uma cena para testar o background, nela o Background se move e loopa infinitamente. Junto com o Background temos um spawner de nuvens que descem na tela

---
+++ Assets/Scripts/FallingEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingEffect : MonoBehaviour
+{
+
+    public float fallingSpeed;
+    [SerializeField]
+    private float destroyTreshold;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += Vector3.down * fallingSpeed;
+        if (transform.position.y < destroyTreshold) Destroy(gameObject);
+    }
+}
