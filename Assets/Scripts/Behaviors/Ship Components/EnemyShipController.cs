96b8d520a5434260374584bee8aeefb161c6c990
brasilia <ksuspec@gmail.com>
2018-12-15 18:24:10 -0200

    Controles das 3 naves inimigas prontos

--- Assets/Scripts/Behaviors/Ship Components/EnemyShipController.cs
+++ Assets/Scripts/Behaviors/Ship Components/EnemyShipController.cs
@@ -16,29 +16,57 @@ public class EnemyShipController : MonoBehaviour
         public AnimationCurve curve;
         public bool loop;
     }
-
+    [Header("Shooting")]
+    public float timeToStartShooting = 0;
+    [Header("Movement")]
     public Vector2 reference = -Vector2.up;
-    public DirectionalTrajectory verticalTrajectory;
     public DirectionalTrajectory horizontalTrajectory;
+    public DirectionalTrajectory verticalTrajectory;
     public float pace = 0.01f;
+    public bool autoMaxPace = true;
+    public float maxPace = 0.05f;
     public float hFrame = 0;
     public float vFrame = 0;
 
     private ShipBHV ship;
+    private float startTime;
     [SerializeField]
     private float frame = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
         ship = GetComponent<ShipBHV>();
+        if (autoMaxPace)
+        {
+            maxPace = ship.speed / Mathf.Min(horizontalTrajectory.length, verticalTrajectory.length);
+        }
+        //Error prevention
+        if (horizontalTrajectory.length == 0)
+        {
+            horizontalTrajectory.length = 1;
+        }
+        if (verticalTrajectory.length == 0)
+        {
+            verticalTrajectory.length = 1;
+        }
+        if (pace == 0 || horizontalTrajectory.curve.length == 0 || verticalTrajectory.curve.length == 0)
+        {
+            maxPace = 0;
+            Debug.Log("Pace and curves must be set. Component might not work properly.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         //Shooting
-        ship.Fire1(); // tries to shoot
+        if (Time.time > startTime + timeToStartShooting)
+        {
+            ship.Fire1(); // tries to shoot
+        }
 
         //Movement
         float distance = 0;
@@ -55,7 +83,7 @@ public class EnemyShipController : MonoBehaviour
             vFrame %= 1;
         }
         Vector3 target = Vector3.zero;
-        while (distance < ship.speed && nextFrame - frame <= 1)
+        while (distance < ship.speed && nextFrame - frame < maxPace)
         {
             nextFrame += pace;
             float nextHFrame = nextFrame / horizontalTrajectory.length;
@@ -77,7 +105,7 @@ public class EnemyShipController : MonoBehaviour
             //    verticalTrajectory.curve.Evaluate(nextFrame / verticalTrajectory.length) - verticalTrajectory.curve.Evaluate(frame / verticalTrajectory.length) * verticalTrajectory.amplitude);
             distance = target.magnitude;
         }
-        transform.position += target.normalized * ship.speed;
+        transform.position += target;
         frame = nextFrame;
     }
 }
