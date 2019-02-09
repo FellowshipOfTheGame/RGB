1449457575163bfc2ed0b92ba6ef3ae793b8b49b
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2019-02-09 00:12:55 -0200

    Adds a lot of juicyness to the main menu
    Adds an animated background to the main menu. 
    Also adds pretty buttons and lets the player move them exactly as changing ships in the game. 
    Adds visual cues to what keys to press and when they are pressed. 
    Adds background music and sound effects for each action in the menu. 
    Changes the credits' window to match the game's aesthetics. 
    Changes the font to OpenDyslexic font to help people read and it was pretty. But we should add it to be optional later.
    
    Note: Q & E navigate through the menu and Select confirm the option.

---
+++ Assets/Scripts/Menu/MenuAudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * A simple audio manager that handles all tracks, how to play them and their basic characteristics
+ */ 
+public class MenuAudioManager : MonoBehaviour
+{
+    //List of all scene's/game's sounds
+    public Sound[] sounds;
+
+    void Awake()
+    {
+
+        //If we turn it into a manager, uncomment
+        //DontDestroyOnLoad(gameObject);
+
+        //Get all sounds in the list, set their source, clip, volume, pitch, loop... it's pretty self-explaining
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    //Handles playing an audio clip. Find them by name, report if nothing is found and play it.
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound: " + name + "not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    //When the menu appears, plays the background music
+    void Start()
+    {
+        Play("MenuBGSnd");
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Struct-like class with the basic characteristics of an audio clip, like the name, file, volume, pitch, loop and source.
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
